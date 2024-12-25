c9c7cb924cf2032de11a5e0e6ab485e0a43a6b29
周睿 <zrufo747@outlook.com>
2024-12-25 06:14:59 +0000

    ok

--- examples/mqttclient/src/main.rs
+++ examples/mqttclient/src/main.rs
@@ -8,28 +8,34 @@ extern crate axstd as std;
 #[cfg(feature = "axstd")]
 extern crate alloc;
 
+use alloc::string::String;
 use core::time::Duration;
+use mqtt_lite::*;
+use std::io::BufRead;
 use std::io::{self, prelude::*};
 use std::net::{TcpStream, ToSocketAddrs};
-use alloc::string::String;
-
-use mqtt_lite::*;
+use std::thread::{sleep, spawn};
 
 #[cfg(feature = "dns")]
 const DEST: &str = "ident.me:80";
 #[cfg(not(feature = "dns"))]
 const DEST: &str = "10.0.0.110:1883";
 
-struct StreamReadIter<'a> {
-    stream: &'a mut TcpStream,
-}
+const LF: u8 = b'\n';
+const CR: u8 = b'\r';
+const DL: u8 = b'\x7f';
+const BS: u8 = b'\x08';
+const SPACE: u8 = b' ';
 
-impl mqtt_lite::BufRead for StreamReadIter<'_> {
+const MAX_CMD_LEN: usize = 256;
+
+struct Socket(TcpStream);
+
+impl mqtt_lite::BufRead for Socket {
     fn read_exact(&mut self, buff: &mut [u8]) -> Result<(), MqttError> {
-        self.stream
+        self.0
             .read_exact(buff)
-            .map_err(|_e| MqttError::Disconnected)?;
-        Ok(())
+            .map_err(|_e| MqttError::Disconnected)
     }
 }
 
@@ -42,7 +48,7 @@ fn client() -> io::Result<()> {
 
     let mut connect = Connect::new("arceos");
 
-    connect.keep_alive = Duration::from_secs(10);
+    connect.keep_alive = Duration::from_secs(65535);
 
     let req = Packet::Connect(connect);
 
@@ -50,11 +56,9 @@ fn client() -> io::Result<()> {
 
     stream.write_all(&connect_bytes).unwrap();
 
-    let mut streamiter = StreamReadIter {
-        stream: &mut stream,
-    };
+    let mut socket = Socket(stream);
 
-    let res = Packet::read_from(&mut streamiter).expect("read connack fail");
+    let res = Packet::read_from(&mut socket).expect("read connack fail");
 
     let ack = match res {
         Packet::ConnAck(ack) => ack,
@@ -63,18 +67,57 @@ fn client() -> io::Result<()> {
 
     println!("connack: {:?}", ack);
 
-    let req = Packet::Publish {
-        dup: false,
-        qos: 0,
-        retain: false,
-        data: Publish::new("test1", Payload::Text(String::from("测试")), None),
-    };
+    let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
 
-    let bytes = req.to_bytes();
+    let mut buf = [0; MAX_CMD_LEN];
+    let mut cursor = 0;
 
-    stream.write_all(&bytes).unwrap();
+    // spawn(|| loop {
+    //     sleep(Duration::from_secs(5));
 
-    Ok(())
+    //     // println!("tick");
+    // });
+
+    loop {
+        if stdin.read(&mut buf[cursor..cursor + 1]).ok() != Some(1) {
+            continue;
+        }
+        if buf[cursor] == b'\x1b' {
+            buf[cursor] = b'^';
+        }
+        match buf[cursor] {
+            CR | LF => {
+                println!();
+                if cursor > 0 {
+                    let req = Packet::Publish {
+                        dup: false,
+                        qos: 0,
+                        retain: false,
+                        data: Publish::new("test1", Payload::Binary(buf[..cursor].to_vec()), None),
+                    };
+
+                    let bytes = req.to_bytes();
+
+                    socket.0.write_all(&bytes).unwrap();
+                    cursor = 0;
+                }
+            }
+            BS | DL => {
+                if cursor > 0 {
+                    stdout.write_all(&[BS, SPACE, BS]).unwrap();
+                    cursor -= 1;
+                }
+            }
+            0..=31 => {}
+            c => {
+                if cursor < MAX_CMD_LEN - 1 {
+                    stdout.write_all(&[c]).unwrap();
+                    cursor += 1;
+                }
+            }
+        }
+    }
 }
 
 #[cfg_attr(feature = "axstd", no_mangle)]
